0390d4794c676c09b3a2f9c21f99349511dd4d8c
jangirsamarth <samarth8947@gmail.com>
2024-09-18 02:04:24 +0530

    Add @vercel/speed-insights dependency and integrate it into RootLayout component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
 
 export const server = "https://coursehub-server.onrender.com";
 
